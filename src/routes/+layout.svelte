16c1d8d89e82fde5bcae06c0d6fbd562dc8e7fe8
Roshanjossey <roshanjossey@gmail.com>
2025-03-10 00:51:04 +0100

    create svelte app with header and footer

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	let { children } = $props();
+    import Header from "../components/Header.svelte"
+    import Footer from "../components/Footer.svelte"
+    import "../app.css"
+    import "devicon/devicon.min.css"
+</script>
+
+<Header />
+
+{@render children()}
+
+<Footer />
